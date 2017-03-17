c8473f2152824eaf7f68f36aad31cf0e4e4028f7
Kay Krewson <kay.k@comcast.net>
2017-03-16 22:04:52 -0700

    fixed a bug in the image update

--- js/amourillo.js
+++ js/amourillo.js
@@ -1,17 +1,16 @@
 /* API Key: ' 432671cad45a2c4cd0b97ddf1fe4adb0
 http://api.brewerydb.com/v2/?apikey=432671cad45a2c4cd0b97ddf1fe4adb0/brewery/KR4X6i
 
-EXAMPLE OF SYNTAX FOR A SINGLE BEER BY NAME
+EXAMPLE OF SYNTAX:
 http://api.brewerydb.com/v2/beers\?key\=432671cad45a2c4cd0b97ddf1fe4adb0\&name\=juniper%20Pale%20Ale
 
 'https://crossorigin.me/http://api.brewerydb.com/v2/beers\?key\=432671cad45a2c4cd0b97ddf1fe4adb0\&name\=amarillo%20Pale%20Ale'
-*/
 
-/*
 syntax for the get
 jQuery.get( url [, data ] [, success ] [, dataType ] )
 $.get
 */
+
 $(document).ready(function() {
   $('select').material_select();
 });
@@ -28,7 +27,7 @@ var beerIdArrayPlain = [];
 
 
 // PULL DATA FROM THE LOCAL DATA FILE
-// Note: beerInfo is the array of objects stored in data.js
+// Note: beerInfo is the array of objects stored in data.js. My plan is to be able to replace it with data pulled from a database when we reach that part of the course - as a way to help myself practice
 function createObjectForGets() {
 
   // CREATE THE ARRAY FOR THE AJAX QUERY BY ID
@@ -59,28 +58,102 @@ createListOfBeers();
 
 // DISPLAY THE CHOSEN BEER'S INFO WHEN CHOSEN IN THE SELECT BOX
 $('#list-of-beers').on('change', function(e) {
-  console.log('lkdjg;ag;dob');
-  // let crossorigin = 'https://crossorigin.me/';
-  // let proxie = 'https://galvanize-cors-proxy.herokuapp.com/';
-  // let beerApi = 'https://api.brewerydb.com/v2/beers/';
-  // let apiKey = '\?key\=432671cad45a2c4cd0b97ddf1fe4adb0';
-  // let params = '\&withBreweries=Y';
-  // let beerId = `\&beerId\=${beerIdArray[randomIndex]}`;
-  // let beerIdQuery = `${proxie}${beerApi}${apiKey}${beerId}${params}`;
+  let selectedBeer = $('#list-of-beers').val();
+  let crossorigin = 'https://crossorigin.me/';
+  let proxy = 'https://galvanize-cors-proxy.herokuapp.com/';
+  let beerApi = 'https://api.brewerydb.com/v2/beers/';
+  let apiKey = '\?key\=432671cad45a2c4cd0b97ddf1fe4adb0';
+  let params = '\&withBreweries=Y';
+  let beerId;
 
-})
+  // GET THE KEY FOR THE SELECTED ITEM (FROM LOCAL DATA STRUCTURE)
+  for (let i = 0; i<beerInfo.length; i++) {
+    if (beerInfo[i].beerName === selectedBeer) {
+      beerId = `\&beerId\=${beerInfo[i].beerId}`;
+    }
+  }
+
+  let beerIdQuery = `${proxy}${beerApi}${apiKey}${beerId}${params}`;
+  console.log(beerIdQuery);
+
+  $.ajax ({
+    method: 'GET',
+    url: beerIdQuery,
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader( 'Access-Control-Allow-Headers', '*' );
+    },
+    success: function(results) {
+      var thisName = results.data.name;
+      var thisBrewer = results.data.breweries[0].name;
+      var thisAbv = results.data.abv;
+      var thisIbu = results.data.ibu;
+      var thisDescription = results.data.description;
+      var backupDescription = results.data.style.description;
+      console.log(results);
+
+      // update the placeholder paragraph
+      if (thisAbv) {
+        if (thisIbu) {
+          // all 4 things get displayed
+          $("#selected-beer-data").html(`<p><b>${thisName}</b></p>` + `<p><i>${thisBrewer}</i></p>` + `<p>ABV: ${thisAbv}</p>` + `<p>IBU: ${thisIbu}</p>`);
+        }
+        else {
+          // beer name, brewery & Abv get displayed
+          $("#selected-beer-data").html(`<p><b>${thisName}</b></p>` + `<p><i>${thisBrewer}</i></p>` + `<p>ABV: ${thisAbv}</p>`);
+        };
+      }
+      else {
+        if (thisIbu) {
+          // beer name, brewery and Ibu get displayed
+          $("#selected-beer-data").html(`<p><b>${thisName}</b></p>` + `<p><i>${thisBrewer}</i></p>` + `<p>IBU: ${thisIbu}</p>`);
+        }
+        else {
+          // only beer name and brewery get displayed
+          $("#selected-beer-data").html(`<p><b>${thisName}</b></p>` + `<p><i>${thisBrewer}</i></p>`);
+        };
+      };
+
+      // update the image label
+      $("#featured-beer-name").remove();
+
+      // update the image
+      if (results.data.labels) {
+        console.log('labels ', results.data.labels);
+        $("#selected-beer-image").attr("src", results.data.labels.medium);
+      }
+      else {
+        $("#selected-beer-image").attr("src", 'images/pint-glass-with-boca-cropped-525x350.png');
+      }
+
+      // UPDATE THE INFORMATION THAT WILL SHOW UPON CARD REVEAL
+      console.log(beerIdQuery);
+
+      $('.activator').on('click', function(e) {
+        if (thisDescription) {
+          $(".card-reveal p").html(`<p><b>${thisName}</b></p>` + `<p>${thisDescription}</p>`);
+        }
+        else {
+          $(".card-reveal p").html(`<p><b>${thisName}</b></p>` + `<p>No description is available for this beer, but you can read about the type: ${backupDescription}</p>`);
+        }
+      });
+    },
+    error: function (error) {
+    }
+  });
+}); // end of function to generate the selected beer
+
 
 
 // GENERATE A NEW FEATURED BEER WHEN THE REPLAY BUTTON IS CLICKED
 $('#featured-beer').on('click', function(e) {
   let randomIndex = Math.floor(Math.random() * (beerIdArray.length-1));
   let crossorigin = 'https://crossorigin.me/';
-  let proxie = 'https://galvanize-cors-proxy.herokuapp.com/';
+  let proxy = 'https://galvanize-cors-proxy.herokuapp.com/';
   let beerApi = 'https://api.brewerydb.com/v2/beers/';
   let apiKey = '\?key\=432671cad45a2c4cd0b97ddf1fe4adb0';
   let params = '\&withBreweries=Y';
   let beerId = `\&beerId\=${beerIdArray[randomIndex]}`;
-  let beerIdQuery = `${proxie}${beerApi}${apiKey}${beerId}${params}`;
+  let beerIdQuery = `${proxy}${beerApi}${apiKey}${beerId}${params}`;
 
   // update the placeholder image
   $.ajax ({
@@ -97,7 +170,6 @@ $('#featured-beer').on('click', function(e) {
       var thisDescription = results.data.description;
       var backupDescription = results.data.style.description;
       console.log(results);
-      // var thisLocation = results.data.breweries.locations.locality + ', '  + results.data.breweries.locations.region;
 
       // update the placeholder paragraph
       if (thisAbv) {
@@ -148,7 +220,3 @@ $('#featured-beer').on('click', function(e) {
     }
   });
 }); // end of function to generate a new featured beer
-
-
-
-// WILL NEED A SORT-BY-STATE DROPDOWN
